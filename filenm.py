57f78dcba3b6ef04a308a1dab02dbbb13a2a6200
krcdavis <32370620+krcdavis@users.noreply.github.com>
2020-04-16 21:37:36 -0700

    filenm.py
    filenm.py

---
+++ filenm.py
@@ -0,0 +1,13 @@
+
+ALLOWED_EXTENSIONS = {'jpeg', 'jpg', 'png'}
+
+def allowed_file(filename):
+        return filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+if __name__ == '__main__':
+        fnm = input("give filename: ")
+        if allowed_file(fnm):
+            print("good")
+        else:
+            print("bad")
